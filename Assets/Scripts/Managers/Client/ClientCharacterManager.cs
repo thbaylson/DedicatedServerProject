08a61821a1ae12364d8d8a85c0b94ff7a7ea26b8
DESKTOP-0DK53VE\thbay <thbaylson@gmail.com>
2024-01-11 23:35:19 -0500

    Lesson: 1C – Authentication. Started work on authentication implementation.

--- Assets/Scripts/Managers/Client/ClientCharacterManager.cs
+++ Assets/Scripts/Managers/Client/ClientCharacterManager.cs
@@ -1,11 +1,34 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 
 public class ClientCharacterManager : MonoBehaviour
 {
     [SerializeField] CreateRequest _createRequest;
+
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync("Player1", "Test1234!");
+        }
+        catch (Exception ex)
+        {
+            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync("Player1", "Test1234!");
+        }
+
+        if (AuthenticationService.Instance.IsAuthorized)
+        {
+            Debug.Log($"Logged in as {AuthenticationService.Instance.PlayerId}");
+        }
+        else
+        {
+            Debug.Log("Failed to login");
+        }
+    }
 
     // This adds custom functionality to the context menu of this component within the editor
     [ContextMenu(nameof(SendCreateRequest))]
