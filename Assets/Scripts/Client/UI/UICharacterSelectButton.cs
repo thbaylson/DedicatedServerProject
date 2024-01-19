ed53b66f3bcde38f6027c57948ef7d76945f7f1a
DESKTOP-0DK53VE\thbay <thbaylson@gmail.com>
2024-01-19 00:44:12 -0500

    Lesson: 1-N – Select Button Icons.

--- Assets/Scripts/Client/UI/UICharacterSelectButton.cs
+++ Assets/Scripts/Client/UI/UICharacterSelectButton.cs
@@ -6,22 +6,43 @@ namespace Client.UI
 {
     class UICharacterSelectButton : MonoBehaviour
     {
-        private PersistedCharacterData _persistedCharacterData;
+        [SerializeField] TMP_Text _characterNameText;
+        [SerializeField] Button _selectButton;
+        [SerializeField] Button _deleteButton;
+        [SerializeField] Image _characterClassIcon;
+        string _characterName;
+
+        private void Awake()
+        {
+            _selectButton.onClick.AddListener(SelectCharacter);
+            _deleteButton.onClick.AddListener(DeleteCharacter);
+        }
 
         private void Start()
         {
             GetComponent<Button>().onClick.AddListener(DeleteCharacter);
         }
 
-        private void DeleteCharacter()
+        private void SelectCharacter() => Debug.Log("Start log in.");
+
+        private void DeleteCharacter() => ClientCharacterManager.Instance.DeleteCharacter(_characterName);
+
+        public void Bind(PersistedCharacterData data)
         {
-            ClientCharacterManager.Instance.DeleteCharacter(_persistedCharacterData.Name);
+            _characterName = data.Name;
+            GetComponentInChildren<TMP_Text>().SetText($"{data.Name} - {data.Class} ({data.Experience})");
+            _characterClassIcon.sprite = PrefabsManager.Instance.GetSprite(data.Class);
         }
 
-        public void Bind(PersistedCharacterData persistedCharacterData)
+        [ContextMenu("Bind To Test")]
+        private void BindToTest()
         {
-            _persistedCharacterData = persistedCharacterData;
-            GetComponentInChildren<TMP_Text>().SetText($"{_persistedCharacterData.Name} - {_persistedCharacterData.Class} ({_persistedCharacterData.Experience})");
+            Bind(new PersistedCharacterData()
+            {
+                Name = "Lorem Ipsum",
+                Class = CharacterClass.Mage.ToString(),
+                Experience = 42
+            });
         }
     }
 }
