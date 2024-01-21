abaa3688d4c3ed784d34eb18f22cf7ee5d076ebc
DESKTOP-0DK53VE\thbay <thbaylson@gmail.com>
2024-01-21 13:33:19 -0500

    Lesson: 2F – Player Script and NetworkVariable Initialization. Created Player class with NetworkVariable fields.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using Unity.Collections;
+using Unity.Netcode;
+
+// This will be shared between Client and Server
+public class Player : NetworkBehaviour
+{
+    // TODO: This should be its own object. Same data as what's found in ServerPlayerManager.
+    public NetworkVariable<FixedString32Bytes> PlayerId;
+    public NetworkVariable<FixedString32Bytes> PlayerName;
+    public NetworkVariable<FixedString32Bytes> CharacterName;
+
+    private void Awake()
+    {
+        // Important: These need to be initialized outside their field declaration or else it'll cause a memory leak
+        // Important (cont.): We init the NetworkVariables, but we can't set their Values yet. It's best to do that in OnNetworkSpawn
+        PlayerId = new();
+        PlayerName = new();
+        CharacterName = new();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            PlayerId.Value = ServerPlayerManager.GetPlayerId(OwnerClientId);
+            PlayerName.Value = ServerPlayerManager.GetPlayerName(OwnerClientId);
+            CharacterName.Value = ServerPlayerManager.GetCharacterName(OwnerClientId);
+        }
+        gameObject.name = $"({PlayerName.Value}) {CharacterName.Value}";
+    }
+}
